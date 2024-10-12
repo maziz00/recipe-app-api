2723aad796d30e4a5212ab22de6f71ab51989274
Mohamed Abdel Aziz <maziz.yahya00@gmail.com>
2024-10-12 20:39:33 +0400

    fix flak8 linting
    Signed-off-by: Mohamed Abdel Aziz <maziz.yahya00@gmail.com>

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,9 +2,11 @@
 Calculator function
 """
 
+
 def add(x, y):
     """ Add x and y then return result """
     return x + y
+
 
 def subtract(x, y):
     """ Subtract x from y then return result """
