28fbb2709f83b61f9b00c666074494a87e8feff8
Mohamed Abdel Aziz <maziz.yahya00@gmail.com>
2024-10-12 16:42:27 +0400

    add & subtract tests
    Signed-off-by: Mohamed Abdel Aziz <maziz.yahya00@gmail.com>

--- app/app/calc.py
+++ app/app/calc.py
@@ -5,3 +5,7 @@ Calculator function
 def add(x, y):
     """ Add x and y then return result """
     return x + y
+
+def subtract(x, y):
+    """ Subtract x from y then return result """
+    return x - y
