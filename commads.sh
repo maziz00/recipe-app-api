ec9387aa3ec58b8491422d9f23c2047fcd677745
Mohamed Abdel Aziz <maziz.yahya00@gmail.com>
2024-10-12 20:19:43 +0400

    check for db configs
    Signed-off-by: Mohamed Abdel Aziz <maziz.yahya00@gmail.com>

--- commads.sh
+++ commads.sh
@@ -5,3 +5,5 @@
 docker-compose run --rm app sh -c "python manage.py startapp core"
 
 docker-compose run --rm app sh -c "python manage.py test"
+
+docker-compose run --rm app sh -c "python manage.py wait_for_db"
