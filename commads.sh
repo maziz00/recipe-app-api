1a223ae04e1b78a7c02e5cdd333ab2f44043e1f2
Mohamed Abdel Aziz <maziz.yahya00@gmail.com>
2024-10-13 19:22:41 +0400

    user models changes
    Signed-off-by: Mohamed Abdel Aziz <maziz.yahya00@gmail.com>

--- commads.sh
+++ commads.sh
@@ -7,3 +7,5 @@ docker-compose run --rm app sh -c "python manage.py startapp core"
 docker-compose run --rm app sh -c "python manage.py test"
 
 docker-compose run --rm app sh -c "python manage.py wait_for_db"
+
+docker-compose run --rm app sh -c "python manage.py wait_for_db && flake8
