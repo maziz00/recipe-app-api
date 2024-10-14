2655e58c8116ec422e0e902b4ab94dd23b26f4fc
Mohamed Abdel Aziz <maziz.yahya00@gmail.com>
2024-10-14 18:40:56 +0400

    working first migrations
    Signed-off-by: Mohamed Abdel Aziz <maziz.yahya00@gmail.com>

--- commads.sh
+++ commads.sh
@@ -8,4 +8,8 @@ docker-compose run --rm app sh -c "python manage.py test"
 
 docker-compose run --rm app sh -c "python manage.py wait_for_db"
 
-docker-compose run --rm app sh -c "python manage.py wait_for_db && flake8
+docker-compose run --rm app sh -c "python manage.py wait_for_db && flake8"
+
+docker-compose run --rm app sh -c "python manage.py makemigrations"
+
+docker-compose run --rm app sh -c "python manage.py wait_for_db && python manage.py migrate"
