8072facb60884601c96f6b164f6a086e7411c3ca
Mohamed Abdel Aziz <maziz.yahya00@gmail.com>
2024-10-14 19:17:08 +0400

    user creation model end
    Signed-off-by: Mohamed Abdel Aziz <maziz.yahya00@gmail.com>

--- commads.sh
+++ commads.sh
@@ -13,3 +13,5 @@ docker-compose run --rm app sh -c "python manage.py wait_for_db && flake8"
 docker-compose run --rm app sh -c "python manage.py makemigrations"
 
 docker-compose run --rm app sh -c "python manage.py wait_for_db && python manage.py migrate"
+
+docker-compose run --rm app sh -c "python manage.py createsuperuser"
