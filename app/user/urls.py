e80b1e6200cce2beae085dba0eeed7ea2099bafa
Mohamed Abdel Aziz <maziz.yahya00@gmail.com>
2024-10-19 06:28:09 +0400

    serializaing part 1
    Signed-off-by: Mohamed Abdel Aziz <maziz.yahya00@gmail.com>

---
+++ app/user/urls.py
@@ -0,0 +1,12 @@
+"""
+Urls mapping fro the user API.
+"""
+from django.urls import path
+
+from user import views
+
+app_name = 'user'
+
+urlpatterns = [
+    path('create/', views.CreateUserView,as_view(), name='create'),
+]
