c4ad2a98e04b0205b616e0ff2e5ce47879526056
Mohamed Abdel Aziz <maziz.yahya00@gmail.com>
2024-10-24 07:26:31 +0400

    token views
    Signed-off-by: Mohamed Abdel Aziz <maziz.yahya00@gmail.com>

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -80,3 +80,21 @@ class PublicUserApiTests(TestCase):
 
         self.assertIn('toke', res.data)
         self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_create_token_bad_credentials(self):
+        """Test returns error if crednetials invalid."""
+        create_user(emial='test@example.com', password='goodpass')
+
+        payload = {'email': 'password': 'badpass'}
+        res = self.client.post(TOKEN_URL, payload)
+
+        self.assertNotIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_token_blank_password(self):
+        """Test posting a blank password return an error."""
+        payload - {'email': 'test@example.com', 'password': ''}
+        res = self.client.post(TOKEN_URL, payload)
+
+        self.assertNotIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
