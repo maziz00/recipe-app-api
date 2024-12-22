e824dd8adee82ffbf6bdaf51d154ec1788e4586b
Mohamed Abdel Aziz <maziz.yahya00@gmail.com>
2024-12-22 11:47:16 +0400

    ingredients api tests

--- app/recipe/tests/test_ingredients_api.py
+++ app/recipe/tests/test_ingredients_api.py
@@ -14,6 +14,10 @@ from recipe.serializers import IngredientSerializer
 
 INGREDIENTS_URL = reverse('recipe:ingredient-list')
 
+def detail_url(ingredient_id):
+    """Create and return" and ingredient detials URL."""
+    return reverse('recipe:ingredient-detail', args=[ingredient_id])
+
 def create_user(email='user@example.com', password='testpass123'):
     """Create and return user."""
     return get_user_model().objects.create_user(email=email, password=password)
